3cf725d469df8268171cd4b3f25ae333a66e8bfe
PIYUSH KUMAR ANAND <piyushanandemail@gamil.com.com>
2025-12-09 22:27:36 +0530

    e2e

---
+++ upcomingcar/upcomingcar/src/main/java/com/renault/upcomingcar/UpcomingcarApplication.java
@@ -0,0 +1,13 @@
+package com.renault.upcomingcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpcomingcarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpcomingcarApplication.class, args);
+	}
+
+}
---
+++ upcomingcar/upcomingcar/src/test/java/com/renault/upcomingcar/UpcomingcarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renault.upcomingcar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpcomingcarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
